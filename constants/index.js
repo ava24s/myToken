0701a6545a25268adfb279969cbda7e762cc6974
ava24 <122343304+ava24s@users.noreply.github.com>
2023-07-30 21:19:43 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xda7daaf9e9851bf8bfb629b64fa4a07d31f992db";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
